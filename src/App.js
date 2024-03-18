ca81781d2f570508088f2aaa943773867bdf8c8c
JANAPATI SAI AJAY <115132468+Matrix726@users.noreply.github.com>
2024-03-18 22:37:25 +0530

    added comments

--- src/App.js
+++ src/App.js
@@ -1,50 +1,61 @@
 /** @format */
+
+// Importing necessary dependencies and components
 import MovieCard from "./components/MovieCard";
 import { useEffect, useState, useRef } from "react";
 import "./App.css";
 import axios from "axios";
 import YouTube from "react-youtube";
+
+// Main function component
 function App() {
+  // Constants for image path and API URL
   const IMAGE_PATH = "https://image.tmdb.org/t/p/w1280";
   const API_URL = "https://api.themoviedb.org/3";
-  const [movies, setMovies] = useState([]);
-  const [selectedMovie, setSelectedMovie] = useState({});
-  const [searchKey, setSearchKey] = useState("");
-  const [playTrailer, setPlayTrailer] = useState(false);
-  const searchRef = useRef(null);
+
+  // State variables
+  const [movies, setMovies] = useState([]); // Movies list
+  const [selectedMovie, setSelectedMovie] = useState({}); // Selected movie details
+  const [searchKey, setSearchKey] = useState(""); // Search query
+  const [playTrailer, setPlayTrailer] = useState(false); // Boolean to control trailer playing
+  const searchRef = useRef(null); // Reference for search input element
+
+  // Function to fetch movies from API
   const fetchMovies = async (searchKey) => {
-    const type = searchKey ? "search" : "discover";
+    const type = searchKey ? "search" : "discover"; // Check if searching or discovering
     const {
       data: { results },
     } = await axios.get(`${API_URL}/${type}/movie`, {
       params: {
-        api_key: process.env.REACT_APP_MOVIE_API_KEY,
-        query: searchKey,
+        api_key: process.env.REACT_APP_MOVIE_API_KEY, // API Key
+        query: searchKey, // Search query
       },
     });
-    setMovies(results);
-    await selectMovie(results[0]);
+    setMovies(results); // Set movies state
+    await selectMovie(results[0]); // Select the first movie in the list
   };
 
+  // Function to fetch details of a single movie
   const fetchMovie = async (id) => {
     const { data } = await axios.get(`${API_URL}/movie/${id}`, {
       params: {
         api_key: process.env.REACT_APP_MOVIE_API_KEY,
-        append_to_response: "videos",
+        append_to_response: "videos", // Append videos to response
       },
     });
     return data;
   };
 
+  // Function to select a movie
   const selectMovie = async (movie) => {
-    setPlayTrailer(false);
-    const data = await fetchMovie(movie.id);
-
-    setSelectedMovie(data);
+    setPlayTrailer(false); // Ensure trailer is closed
+    const data = await fetchMovie(movie.id); // Fetch details of selected movie
+    setSelectedMovie(data); // Set selected movie state
   };
 
+  // useEffect to fetch initial movies and add event listener for Ctrl+K
   useEffect(() => {
-    fetchMovies();
+    fetchMovies(); // Fetch initial movies
     const handleKeyPress = (event) => {
       if (event.ctrlKey && event.key === "k") {
         // Focus on the search input when Ctrl+K is pressed
@@ -52,49 +63,52 @@ function App() {
         event.preventDefault(); // Prevent default behavior of Ctrl+K
       }
     };
-
-    document.addEventListener("keydown", handleKeyPress);
-
+    document.addEventListener("keydown", handleKeyPress); // Add event listener
     return () => {
-      document.removeEventListener("keydown", handleKeyPress);
+      document.removeEventListener("keydown", handleKeyPress); // Remove event listener on cleanup
     };
   }, []);
 
+  // Function to render movie cards
   const renderMovies = () =>
     movies.map((movie) => (
       <MovieCard key={movie.id} movie={movie} selectMovie={selectMovie} />
     ));
 
+  // Function to handle search submission
   const searchMovies = (e) => {
-    e.preventDefault();
-    fetchMovies(searchKey);
+    e.preventDefault(); // Prevent form submission
+    fetchMovies(searchKey); // Fetch movies based on search query
   };
 
+  // Function to render YouTube trailer
   const renderTrailer = () => {
     const trailer = selectedMovie.videos.results.find(
       (vid) => vid.name === "Official Trailer"
-    );
-    const key = trailer ? trailer.key : selectedMovie.videos.results[0].key;
+    ); // Find official trailer
+    const key = trailer ? trailer.key : selectedMovie.videos.results[0].key; // Get trailer key
     return (
       <YouTube
         videoId={key}
         containerClassName={"youtube-container"}
         opts={{
           width: "100%",
-
           playerVars: {
-            autoplay: 1,
-            controls: 0,
+            autoplay: 1, // Autoplay trailer
+            controls: 0, // Hide controls
           },
         }}
       />
     );
   };
+
+  // JSX return
   return (
     <div className="App">
       <header className={"header"}>
         <div className={"header-content max-center"}>
           <h1>Movie Trailer App</h1>
+          {/* Search form */}
           <form onSubmit={searchMovies}>
             <input
               ref={searchRef}
@@ -109,6 +123,7 @@ function App() {
           </form>
         </div>
       </header>
+      {/* Hero section */}
       <div
         className="hero"
         style={{
@@ -116,6 +131,7 @@ function App() {
         }}
       >
         <div className="hero-content max-center">
+          {/* Close button */}
           {playTrailer ? (
             <button
               className={"button button-close"}
@@ -125,16 +141,20 @@ function App() {
             </button>
           ) : null}
 
+          {/* Play trailer button */}
           <button className={"button"} onClick={() => setPlayTrailer(true)}>
             Play Trailer
           </button>
+          {/* Movie details */}
           <h1 className={"hero-title"}>{selectedMovie.title}</h1>
           {selectedMovie.overview ? (
             <p className={"hero-overview"}> {selectedMovie.overview}</p>
           ) : null}
         </div>
       </div>
+      {/* Render trailer if selected movie has videos */}
       {selectedMovie.videos && playTrailer ? renderTrailer() : null}
+      {/* Movies container */}
       <div className="container max-center">{renderMovies()}</div>
     </div>
   );
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,10 +2,20 @@
 
 import React from "react";
 
+/**
+ * Functional component representing a movie card.
+ * @param {Object} props - The props object.
+ * @param {Object} props.movie - The movie object containing details like title, poster_path, etc.
+ * @param {Function} props.selectMovie - Callback function to handle selection of a movie.
+ * @returns {JSX.Element} Movie card component.
+ */
 const MovieCard = ({ movie, selectMovie }) => {
+  // Base URL for movie poster images
   const IMAGE_PATH = "https://image.tmdb.org/t/p/w500";
+
   return (
     <div className={"movie-card"} onClick={() => selectMovie(movie)}>
+      {/* Render movie poster if available, otherwise show placeholder */}
       {movie.poster_path ? (
         <img
           className={"movie-cover"}
